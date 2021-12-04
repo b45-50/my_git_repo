5e24503e41eb6dad4783e59524f2153b3687e112
hozdemir33 <hozdemir8233@gmail.com>
2021-12-04 12:41:43 -0800

    first commit

---
+++ FirstJavaClass.java
@@ -0,0 +1,21 @@
+package my_git_repo;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+
+	
+	add(3,4);
+	
+	System.out.println(add(3,4));
+		
+	}
+	
+	public static int add( int a, int b) {
+		
+		return a+b;
+			
+	}
+	
+
+}
