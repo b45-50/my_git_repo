c1681218aff32669dd6ca88732aed5d394a40097
hozdemir33 <hozdemir8233@gmail.com>
2021-12-04 18:02:51 -0800

    updated my code

--- SecondJavaClass.java
+++ SecondJavaClass.java
@@ -1,5 +1,22 @@
 package my_git_repo;
 
+
 public class SecondJavaClass {
+
+	
+	
+	public static void main(String[] args) {
+		
+	
+	System.out.println(multiplication(3,4));
+		
+		
+	}
+	
+	public static int multiplication(int k , int m) {
+		
+		return k*m;
+		
+	}
 
 }
