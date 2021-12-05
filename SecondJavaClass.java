5cd5d0ffbd3e99b84b8301ca2ebc85758004bfd7
hozdemir33 <hozdemir8233@gmail.com>
2021-12-04 18:22:05 -0800

    deleted unness. code

--- SecondJavaClass.java
+++ SecondJavaClass.java
@@ -9,22 +9,14 @@ public class SecondJavaClass {
 		
 	
 	System.out.println(multiplication(3,4));
-	System.out.println(addition( 5,6));
-	System.out.println();
-		
-		
+	
+	
 	}
 	
 	public static int multiplication(int k , int m) {
 		
 		return k*m;
 		
-	}
-	
-	public static int addition( int m, int h) {
-		
-		
-		return m+h;
 		
 	}
 
